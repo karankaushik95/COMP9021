9c8fbaf3daa9a4ceff11f7a7dbe2f31bbd26580f
Karan Kaushik <kkricardokaka95@gmail.com>
2018-04-14 19:15:39 +1000

    Update and rename assignmentq3.py to assignment1q3.py

--- assignment1q3.py
+++ assignment1q3.py
@@ -34,22 +34,26 @@ rectangles = [x.lstrip() for x in rectangles]
 
 rect_dict = dict()
 
+
 '''
 Function to check if a rectange is completely enclosed by another rectangle,
 i.e to see if it will not contribute to the perimeter(solution) at all
 '''
-def enclosed_by(check_rectangle, rect_dict):
-    edge_count = 0
+def enclosed_by(check_rectangle, rect_dict, enclosed_rects):
+
     for key,value in rect_dict.items():
         if check_rectangle == key:
             continue
         else:
-            '''
-            Write code to check each edge with edges in the current key.........
-            '''
-            pass
-    if edge_count == 4:
-        rect_dict.pop(check_rectangle)    
+            top_left = key[1]
+            bottom_right = key[3]
+            check_left = check_rectangle[1]
+            check_right = check_rectangle[3]
+            if (top_left[0] < check_left[0] and check_left[0] < bottom_right[0]) and (top_left[0] < check_right[0] and check_right[0] < bottom_right[0]):
+                if (top_left[1] > check_left[1] and check_left[1] > bottom_right[1]) and (top_left[1] > check_right[1] and check_right[1] > bottom_right[1]):
+                    if check_rectangle not in enclosed_rects:
+                        enclosed_rects.append(check_rectangle)
+             
     
   
 '''
@@ -57,7 +61,7 @@ Standard function to implement distance formula to calculate distance between tw
 '''    
 
 def distance_formula(source_point, destination_point):
-    return (sqrt((source_point[0] - destination_point[0])**2 + (source_point[1]- destination_point[1])**2))
+    return int((sqrt((source_point[0] - destination_point[0])**2 + (source_point[1]- destination_point[1])**2)))
 
 
 def parse_file(rectangles):
@@ -65,7 +69,7 @@ def parse_file(rectangles):
         x1,y1,x2,y2 = rectangle_edges.split(' ')
         edge_co_ordinates = get_corner_points_of_rectangle(int(x1),int(y1),int(x2),int(y2))
         rect_dict[edge_co_ordinates] = get_all_points_of_rectangle(edge_co_ordinates) 
-        #get_all_points_of_rectangle(x1,y1,x2,y2)
+        
 
 
 '''
@@ -74,11 +78,56 @@ Function to get all vertices of the rectangle
 def get_corner_points_of_rectangle(x1,y1,x2,y2):
     return((x1,y1), (x1,y2), (x2,y2), (x2,y1))
 
+'''
+Finds out of a point is inside or outside a given rectangle
+inside iff its x coordinate is between the x of the top left vertex and bottom right vertex
+AND iff its y coordinate is between the y of bottom right vertex and top left vertex
+'''
+def inside_or_outside(point, rectangles):
+    for key,value in rectangles.items():
+        top_left = key[1]
+        bottom_right = key[3]
+        if top_left[0] < point[0] and point[0] < bottom_right[0]:
+            if top_left[1] > point[1] and point[1] > bottom_right[1]:
+                return True #implies point is inside another rectangle so you dont count the current start and end
+    return False
+   
+'''
+Function to get contributing perimeter of a rectangle
+'''
+def get_perimeter(edge,rectangles):
+   # print(edge)
+    peri = [] ##useless temporary variable
+    #print(rectangles)
+    current_perimeter = 0
+    start = edge[0]
+    #print(start)
+    for item in edge:
+        for key, value in rectangles.items():
+            for edges in value:
+                if item in edges:
+                    #print("start", start)
+                    #print("intersected edge", item)
 
-def get_perimeter(rectangles):
-    pass
-
-
+                    if inside_or_outside(start, rectangles):
+                        start = item
+                        end = None ##means current edge portion is actually inside another rectangle  
+                    else: ##now what if not eh?
+                        peri.append((start, item))
+                        current_perimeter += distance_formula(start, item)
+                else:
+                    end = item
+                    
+    end = edge[len(edge)-1]
+    #print(start,end)
+    if not inside_or_outside(end, rectangles):
+        peri.append((start, item))
+        current_perimeter += distance_formula(start, end) 
+    #print(peri)
+    return current_perimeter           
+'''
+There could be 2 intersections what about that? HANDLED YOOOOOOOOOOO  
+'''
 '''
 Get all points in a line given the endpoints
 '''
@@ -91,13 +140,14 @@ def get_points_on_a_line(x1 ,y1 , x2, y2 ):
         else:
             for i in range(y1,y2+1):
                 line.append((x1,i))
-    else:
+    elif y1==y2:
         if x1>x2:
             for i in range(x2,x1+1):
                 line.append((i,y1))                        
         else:
             for i in range(x1,x2+1):
                 line.append((i,y1)) 
+    #print(line)
     return line
              
 '''
@@ -106,24 +156,56 @@ Function to get all points on the boundary of the rectangle including the vertic
 
 def get_all_points_of_rectangle(edge_coordinates):
    coordinates = []
-   for i in range(0, len(edge_coordinates)):
+   #print("edges", edge_coordinates)
+   for i in range(0, len(edge_coordinates)-1):
        edge = [] 
-       for j in range(i, len(edge_coordinates)):
+       #print("Current vertex",edge_coordinates[i][0], edge_coordinates[i][1])
+       for j in range(i+1, len(edge_coordinates)):
+           #print("Check vertex",edge_coordinates[j][0], edge_coordinates[j][1])
+           
            if edge_coordinates[i][0] != edge_coordinates[j][0] and edge_coordinates[i][1] != edge_coordinates[j][1]:
+               #print("opposite for current j")
+               #print()
                continue
            elif edge_coordinates[i][0] == edge_coordinates[j][0]:
                edge = get_points_on_a_line(edge_coordinates[i][0], edge_coordinates[i][1], edge_coordinates[j][0], edge_coordinates[j][1])
+               #print("x same for current j")
+               #print()
            elif edge_coordinates[i][1] == edge_coordinates[j][1]:
                edge = get_points_on_a_line(edge_coordinates[i][0], edge_coordinates[i][1], edge_coordinates[j][0], edge_coordinates[j][1])
-       coordinates.append(edge)    
+               #print("y same for current j")
+               #print()
+           coordinates.append(edge)    
    #print(coordinates)            
    return coordinates
 
 parse_file(rectangles)
 
+    
+perimeter = 0
+enclosed_rects = []
+   
 
 
-#test = ((-15,0), (-15,10), (5,10), (5,0))
+
+for key,value in rect_dict.items():
+    enclosed_by(key,rect_dict, enclosed_rects)
+
+for item in enclosed_rects:
+    rect_dict.pop(item)
+
 #for key,value in rect_dict.items():
-    #enclosed_by(key, rect_dict)
+   # print(key)
+    #print()
 
+for key,value in rect_dict.items():
+    edges = rect_dict[key]    
+    #print(l[0])
+    temp_dict = dict(rect_dict)
+    temp_dict.pop(key)
+    #print(edges)
+    for edge in edges:
+        perimeter += get_perimeter(edge, temp_dict)    
+    
+    #call perimeter here? 
+print("The perimeter is:",perimeter)
